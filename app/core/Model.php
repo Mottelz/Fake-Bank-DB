975a92d0a2b9403bd7d6059d1583d9f760f6ca7f
Mottelz <mottelz@users.noreply.github.com>
2018-11-21 21:37:23 -0500

    Started working on db to php connection. Need to test this.

---
+++ app/core/Model.php
@@ -0,0 +1,59 @@
+<?php
+//This class is used so that all of the files in the models directory can access the database without the need to rewrite the same basic functions repeatedly.
+class Model {
+    public  $connection;
+    public $db_host;
+    public  $db_user;
+    public  $db_pword;
+    public  $db_name;
+
+     function __construct() {
+         $this->db_host = "udc353.encs.concordia.ca";
+         $this->db_user = "udc353_2";
+         $this->db_pword = "danger";
+         $this->db_name = "udc353_2";
+     }
+
+     //connect to the server
+     private function connectDB() {
+         $this->connection = new mysqli($this->db_host, $this->db_user, $this->db_pword, $this->db_host);
+         if ($this->connection->connect_errno) {
+             printf("Connect failed: %s\n", $this->connection->connect_error);
+             return false;
+         } else {
+             return true;
+         }
+     }
+
+     //disconnect from the server
+     private function disconnectDB() {
+         mysqli_close($this->connection);
+     }
+
+     //get the data
+     public function getData($query) {
+         $results = array();
+         $this->connectDB();
+         if($result = $this->connection->query($query)) {
+             while ($row = $result->fetch_object()) {
+                 $results[] = $row;
+             }
+             $result->close();
+             $this->disconnectDB();
+             return $results;
+         };
+     }
+
+     //update the data
+     public function updateData($query) {
+         $this->connectDB();
+         if($this->connection->query($query)===true) {
+             $this->disconnectDB();
+             return true;
+         } else {
+             $this->disconnectDB();
+             return false;
+         }
+     }
+}
+?>
--- app/models/AccountModel.php
+++ app/models/AccountModel.php
@@ -1,39 +1,39 @@
 <?php
 
-class AccountModel 
+class AccountModel extends Model
 {
 	public $table = 'Account';
 	public $accounts;
 
-	private $data = 
-	'{
-		"Account" : [{
-			"account_id" : 1,
-			"client_id" : 1,
-			"option" : "Option1",
-			"type" : "Checking",
-			"service" : "Service1",
-			"balance" : 1000
-		}, {
-			"account_id" : 2,
-			"client_id" : 1,
-			"option" : "Option2",
-			"type" : "Savings",
-			"service" : "Service2",
-			"balance" : 2000
-		}, {
-			"account_id" : 3,
-			"client_id" : 1,
-			"option" : "Option3",
-			"type" : "Investment",
-			"service" : "Service3",
-			"balance" : 3000
-		}]
-	}';
+	private $data;
+//	'{
+//		"Account" : [{
+//			"account_id" : 1,
+//			"client_id" : 1,
+//			"option" : "Option1",
+//			"type" : "Checking",
+//			"service" : "Service1",
+//			"balance" : 1000
+//		}, {
+//			"account_id" : 2,
+//			"client_id" : 1,
+//			"option" : "Option2",
+//			"type" : "Savings",
+//			"service" : "Service2",
+//			"balance" : 2000
+//		}, {
+//			"account_id" : 3,
+//			"client_id" : 1,
+//			"option" : "Option3",
+//			"type" : "Investment",
+//			"service" : "Service3",
+//			"balance" : 3000
+//		}]
+//	}';
 
-	function __construct()
-	{
-		$this->accounts = json_decode($this->data)->Account;
+	function __construct() {
+//		$this->accounts = json_decode($this->data)->Account;
+        return $this->getData("SELECT * FROM Account");
 	}
 }
 
