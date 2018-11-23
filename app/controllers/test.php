2499438aba6d5c35d999a49777b1bcf03f8501db
Markisino <tropiano.m@gmail.com>
2018-11-23 16:48:05 -0500

    Update test.php

--- app/controllers/test.php
+++ app/controllers/test.php
@@ -6,9 +6,9 @@ TEST
 
 <body>
   <?php
-  include './app/views/login.php';
-  include './app/core/Model.php';
-  include './app/models/ClientModel.php';
+  //include './app/views/login.php';
+  //include './app/core/Model.php';
+  //include './app/models/ClientModel.php';
   echo "THIS IS A TEST PAGE";
   $clientModel = $this->model('ClientModel');
   echo $clientModel->getClientById(1);
