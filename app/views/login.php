0b9b19ffd86755bde15f3a2b162fa48c8fbaca0e
Timothy <timmy_258@hotmail.com>
2018-11-23 23:43:31 -0500

    Progress on DB connection

--- app/views/login.php
+++ app/views/login.php
@@ -49,7 +49,7 @@
           <input type="checkbox" value="remember-me"> Remember me
         </label>
       </div>
-      <button class="btn btn-primary" type="submit">Sign in</button>
+      <button class="btn btn-primary" name="login" type="submit">Sign in</button>
     </form>
 
   </div>
@@ -88,7 +88,6 @@
 
 <?php
   echo "THIS IS A TEST PAGE";
-  include "./app/core/Controller.php";
   $clientModel = $this->model('ClientModel');
   echo $clientModel->getClientById(1);
 ?>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once 'app/core/App.php';
 require_once 'app/core/Controller.php';
+require_once 'app/core/Model.php';
 
 $app = new App();
 
