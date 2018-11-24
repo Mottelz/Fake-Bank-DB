519c06b2039666a4ab5e01f158f422d1b359dab4
Timothy <timmy_258@hotmail.com>
2018-11-23 23:53:55 -0500

    Test

--- app/core/Model.php
+++ app/core/Model.php
@@ -2,21 +2,20 @@
 //This class is used so that all of the files in the models directory can access the database without the need to rewrite the same basic functions repeatedly.
 class Model {
     public  $connection;
-    public $db_host;
-    public  $db_user;
-    public  $db_pword;
-    public  $db_name;
+    public $db_host = "udc353.encs.concordia.ca";
+    public  $db_user = "udc353_2";
+    public  $db_pword = "danger";
+    public  $db_name = "udc353_2";
 
-     function __construct() {
-         $this->db_host = "udc353.encs.concordia.ca";
-         $this->db_user = "udc353_2";
-         $this->db_pword = "danger";
-         $this->db_name = "udc353_2";
-     }
+     // function __construct() {
+     //     $this->db_host = "udc353.encs.concordia.ca";
+     //     $this->db_user = "udc353_2";
+     //     $this->db_pword = "danger";
+     //     $this->db_name = "udc353_2";
+     // }
 
      //connect to the server
      private function connectDB() {
-        var_dump($this->db_host);
          $this->connection = new mysqli($this->db_host, $this->db_user, $this->db_pword, $this->db_host);
          if ($this->connection->connect_errno) {
              printf("Connect failed: %s\n", $this->connection->connect_error);
