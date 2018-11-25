45aeec533975270b0de15be70484da0d5484725c
Timothy <timmy_258@hotmail.com>
2018-11-25 10:54:09 -0500

    Update signup.php

--- app/controllers/signup.php
+++ app/controllers/signup.php
@@ -7,7 +7,7 @@ class signup extends Controller
 		$this->checkSignupData();
 
 		$branchModel = $this->model('BranchModel');
-		$branches = $branchModel->branches;
+		$branches = $branchModel->getAllBranches();
 
 		$this->view('signup', ['branches' => $branches]);
 	}
@@ -17,6 +17,7 @@ class signup extends Controller
 		if(isset($_POST['signup']) && $this->validateSignupData())
 		{
 			//NEED CREATE CLIENT QUERY
+
 			header("Location:/login");
 		}
 	}
@@ -24,6 +25,19 @@ class signup extends Controller
 	public function validateSignupData()
 	{
 		//INSERT VALIDATION (AS NEEDED)
+		$streetaddressregex = '(^\d+\s[a-zA-Z0-9\-\'\s]+$)';
+		$postalcoderegex = '(^[a-zA-Z]{1}\d{1}[a-zA-Z]{1}\s*\d{1}[a-zA-Z]{1}\d{1}$)';
+		$phoneregex = '(^\(*\d{3}\)*\s*-*\s*\d{3}\s*-*\s*\d{4}$)';
+		$emailregex = '(^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$)';
+
+		if(!preg_match($streetaddressregex, $_POST['street_address']))
+			return false;
+		if(!preg_match($postalcoderegex, $_POST['postal_code']))
+			return false;
+		else if(!preg_match($phoneregex, $_POST['phone']))
+			return false;
+		else if(!preg_match($emailregex, $_POST['email']))
+			return false;
 
 		return true;
 	}
