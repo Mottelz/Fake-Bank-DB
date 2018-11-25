0bb456d322639d98485aecaee27d2a042afe8aec
Mottelz <mottelz@users.noreply.github.com>
2018-11-25 00:55:11 -0500

    Update profile.php

--- app/views/profile/contactInformation.php
+++ app/views/profile/contactInformation.php
@@ -24,13 +24,6 @@
 </head>
 
 <body>
-<p><?php var_dump($data)?></p>
-<br>
-<p><?php var_dump($address)?></p>
-<br>
-<p><?php var_dump($countryT)?></p>
-<br>
-<p><?php var_dump($user)?></p>
 	<div class="templateux-cover" style="background-image: url(images/slider-1.jpg);resize:verticle;overflow:auto;">
 		<div class="container">
 			<div class="col-md-8">
