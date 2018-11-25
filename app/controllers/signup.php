2d86e6e32646cdbc024a07c94a597828eb731bbf
Timothy <timmy_258@hotmail.com>
2018-11-25 14:12:12 -0500

    server-side validation
    added simple birth date validation

--- app/controllers/signup.php
+++ app/controllers/signup.php
@@ -40,15 +40,19 @@ class signup extends Controller
 
 	public function validateSignupData()
 	{
-		//INSERT VALIDATION (AS NEEDED)
 		$streetaddressregex = '(^\d+\s[a-zA-Z0-9\-\'\s]+$)';
 		$postalcoderegex = '(^[a-zA-Z]{1}\d{1}[a-zA-Z]{1}\s*\d{1}[a-zA-Z]{1}\d{1}$)';
 		$phoneregex = '(^\d{10}$)';
 		$emailregex = '(^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$)';
 
-		if(!preg_match($streetaddressregex, $_POST['street_address']))
+		$now = date('Y/m/d');
+		$birthDate = date('Y/m/d', strtotime($_POST['birth_date']));
+
+		if($birthDate >= $now)
 			return false;
-		if(!preg_match($postalcoderegex, $_POST['postal_code']))
+		else if(!preg_match($streetaddressregex, $_POST['street_address']))
+			return false;
+		else if(!preg_match($postalcoderegex, $_POST['postal_code']))
 			return false;
 		else if(!preg_match($phoneregex, $_POST['phone']))
 			return false;
