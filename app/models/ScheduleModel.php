212eeefed676a6e2ab7c61eb3902f6f31ba1a9bd
Timothy <timmy_258@hotmail.com>
2018-11-25 17:53:48 -0500

    Update ScheduleModel.php

--- app/models/ScheduleModel.php
+++ app/models/ScheduleModel.php
@@ -18,7 +18,7 @@ class ScheduleModel extends Model
     }
 
     public function insertSchedule($id, $employee_id, $sched_type, $sched_date) {
-    	$this->insertData("INSERT INTO Schedule (Sched_id, Employee_id, Sched_type, Sched_date VALUES (" .
+    	$this->insertData("INSERT INTO Schedule (Sched_id, Employee_id, Sched_type, Sched_date) VALUES (" .
             $id .
             ", " . $employee_id .
             ", '" . $sched_type . "'" .
