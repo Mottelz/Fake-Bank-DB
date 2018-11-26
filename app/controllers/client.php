55fb069907dcc73ee54db34be0471a1eb04ed786
Timothy <timmy_258@hotmail.com>
2018-11-26 00:23:34 -0500

    Client Done

--- app/controllers/client.php
+++ app/controllers/client.php
@@ -149,14 +149,38 @@ class client extends Controller
 
 	public function validateAddClientData()
 	{
-		//INSERT VALIDATION (AS NEEDED)
+		$streetaddressregex = '(^\d+\s[a-zA-Z0-9\-\'\s]+$)';
+		$postalcoderegex = '(^[a-zA-Z]{1}\d{1}[a-zA-Z]{1}\s*\d{1}[a-zA-Z]{1}\d{1}$)';
+		$phoneregex = '(^\(*\d{3}\)*\s*-*\s*\d{3}\s*-*\s*\d{4}$)';
+		$emailregex = '(^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$)';
+
+		if(!preg_match($streetaddressregex, $_POST['street_address']))
+			return false;
+		if(!preg_match($postalcoderegex, $_POST['postal_code']))
+			return false;
+		else if(!preg_match($phoneregex, $_POST['phone']))
+			return false;
+		else if(!preg_match($emailregex, $_POST['email']))
+			return false;
 
 		return true;
 	}
 
 	public function validateEditClientData()
 	{
-		//INSERT VALIDATION (AS NEEDED)
+		$streetaddressregex = '(^\d+\s[a-zA-Z0-9\-\'\s]+$)';
+		$postalcoderegex = '(^[a-zA-Z]{1}\d{1}[a-zA-Z]{1}\s*\d{1}[a-zA-Z]{1}\d{1}$)';
+		$phoneregex = '(^\(*\d{3}\)*\s*-*\s*\d{3}\s*-*\s*\d{4}$)';
+		$emailregex = '(^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$)';
+
+		if(!preg_match($streetaddressregex, $_POST['street_address']))
+			return false;
+		if(!preg_match($postalcoderegex, $_POST['postal_code']))
+			return false;
+		else if(!preg_match($phoneregex, $_POST['phone']))
+			return false;
+		else if(!preg_match($emailregex, $_POST['email']))
+			return false;
 
 		return true;
 	}
