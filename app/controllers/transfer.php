228f4636bfaf47218b8ade225802d69f5800e086
Anthony <funiciello.anthony@gmail.com>
2018-11-25 23:30:02 -0500

    Update transfer.php

--- app/controllers/transfer.php
+++ app/controllers/transfer.php
@@ -46,12 +46,12 @@ class transfer extends Controller
 		if($_SESSION['login_type'] == 'Client')
 		{
 			$transactionModel = $this->model('TransactionModel');
-			$transactions = $transactionModel-getTransferByClientId($_SESSION['login_id']);
+			$transactions = $transactionModel->getTransferByClientId($_SESSION['login_id']);
 		}
 		else //$_SESSION['login_type'] == 'Employee'
 		{
 			$transactionModel = $this->model('TransactionModel');
-					$transactions = $transactionModel-getTransferByClientId($_SESSION['login_id']);
+				$transactions = $transactionModel->getTransferByClientId($_SESSION['login_id']);
 		}
 
 		$this->view('transfer/transferHistory', ['transactions' => $transactions]);
