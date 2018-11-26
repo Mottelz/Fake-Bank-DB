7434c22940c78d5265640188982e1906c4912ed3
Timothy <timmy_258@hotmail.com>
2018-11-26 00:42:06 -0500

    Update account.php

--- app/controllers/account.php
+++ app/controllers/account.php
@@ -93,7 +93,7 @@ var_dump($_POST);
 				$accountModel->addAccountById($account_id, $client_id,
 				$Account_Option, $Account_Type, $Account_Service,
 				$Charge_Plan_Option, $Interest_Rate_Type, $Interest_Rate_Service );
-
+			}
 			else //$_SESSION['login_type'] == 'Employee'
 			{
 				//NEED CREATE ACCOUNT QUERY (FOR EMPLOYEE)
