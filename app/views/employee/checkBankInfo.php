890fa67e2a6b2aae24fd649fd4d948f17c8b21a4
Anthony <funiciello.anthony@gmail.com>
2018-11-26 22:25:15 -0500

    Update checkBankInfo.php

--- app/views/employee/checkBankInfo.php
+++ app/views/employee/checkBankInfo.php
@@ -17,56 +17,9 @@
 	<div><h2>Bank Summary</h2></div>
 
 	<br />
-
-	<div><a href="/employee/add"><button type="button" class="btn btn-outline-success">Add Employee</button></a></div>
-
 	<br />
 
-	<table class="table table-striped">
-		<thead>
-			<tr>
-			<th>Employee #</th>
-			<th>Branch #</th>
-			<th>Department</th>
-			<th>Name</th>
-			<th>Title</th>
-			<th>Salary</th>
-			<th>Start Date</th>
-			<th>Active</th>
-		</tr>
-	</thead>
-	<tbody>
-	<?php
-		for($index = 0; $index < count($employees); $index++)
-		{
-			$employee = $employees[$index];
-			$employee_id = $employee->Employee_id;
-			$branch_id = $employee->Branch_id;
-			$department = $employee->Department;
-			$first_name = $employee->First_name;
-			$last_name = $employee->Last_name;
-			$title = $employee->Title;
-			$salary = $employee->Salary;
-			$start_date = $employee->Employee_Start_date;
-			$active = $employee->Active;
-	?>
-			<tr>
-				<td><?= $employee_id ?></td>
-				<td><?= $branch_id ?></td>
-				<td><?= $department ?></td>
-				<td><?= "$first_name $last_name" ?></td>
-				<td><?= $title ?></td>
-				<td><?= $salary ?></td>
-				<td><?= $start_date ?></td>
-				<td><?= $active == true ? 'Yes' : 'No' ?></td>
-				<td><a href="/employee/details/<?= $employee_id ?>">View Details</a></td>
-				<td><a href="/employee/edit/<?= $employee_id ?>">Edit</a></td>
-			</tr>
-	<?php
-		}
-	?>
 
-	</table>
 </div>
 </div>
 </div>
