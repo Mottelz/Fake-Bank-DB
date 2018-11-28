5f975a731437fb06b377a84fe7b2bfd77dc181ab
Anthony Funiciello <37711422+anthroe@users.noreply.github.com>
2018-11-28 16:31:47 -0500

    Update checkBankInfo.php

--- app/views/employee/checkBankInfo.php
+++ app/views/employee/checkBankInfo.php
@@ -1,6 +1,5 @@
 <?php
 	$monthlyPayroll = $data['monthlyPayroll'];
-	$monthlyPayroll = $data['monthlyPayroll'];
 	$monthlyPayrollByCity = $data['monthlyPayrollByCity'];
 	$monthlyPayrollByBranch = $data['monthlyPayrollByBranch'];
 	$annualLosses = $data['montannualLosseshlyPayroll'];
@@ -61,6 +60,82 @@
 				?>
 			</select>
 			<h3><?= $annualLossesByBranch ?></h3>
+
+
+
+
+
+			<h2>Get Profits By City:</h2>
+		<select name="city" required>
+				<option selected disabled value="">---Select Account---</option>
+				<?php
+
+					for($index = 0; $index < count($city); $index++)
+					{
+						$something = $city[$index];
+						$CityName = $something->City;
+				?>
+						<option value="<?= $CityName ?>"><?= $CityName ?></option>
+				<?php
+					}
+				?>
+			</select>
+			<h3><?= $annualProfitsByCity ?></h3>
+	<h2>Get Profits By Branch:</h2>
+		<select name="branch" required>
+				<option selected disabled value="">---Select Account---</option>
+				<?php
+
+					for($index = 0; $index < count($branches); $index++)
+					{
+						$branch = $branches[$index];
+						$branchName = $branch->Branch_id;
+				?>
+						<option value="<?= $branchName ?>"><?= $branchName ?></option>
+				<?php
+					}
+				?>
+			</select>
+			<h3><?= $annualProfitsByBranch ?></h3>
+
+
+
+
+
+
+
+			<h2>Get Monthly Payroll By City:</h2>
+		<select name="city" required>
+				<option selected disabled value="">---Select Account---</option>
+				<?php
+
+					for($index = 0; $index < count($city); $index++)
+					{
+						$something = $city[$index];
+						$CityName = $something->City;
+				?>
+						<option value="<?= $CityName ?>"><?= $CityName ?></option>
+				<?php
+					}
+				?>
+			</select>
+			<h3><?= $annualLossesByCity ?></h3>
+	<h2>Get Monthly Payroll By Branch:</h2>
+		<select name="branch" required>
+				<option selected disabled value="">---Select Account---</option>
+				<?php
+
+					for($index = 0; $index < count($branches); $index++)
+					{
+						$branch = $branches[$index];
+						$branchName = $branch->Branch_id;
+				?>
+						<option value="<?= $branchName ?>"><?= $branchName ?></option>
+				<?php
+					}
+				?>
+			</select>
+			<h3><?= $monthlyPayrollByCity ?></h3>
 	<br />
 	<br />
 		<div>
